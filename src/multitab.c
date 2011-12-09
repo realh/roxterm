realh/roxterm

commit 2d9bc1b8174145c1fcae7213c6b22cd77adfd482

* Prevent crash when dragging lone tab
Dragging a window's only tab to another window caused a crash
because the roxterm data's win field was being updated too late.
Now always read via tab's accessor.

src/multitab.c
@@ -241,6 +241,7 @@ MultiTab *multi_tab_new(MultiWin * parent, gpointer user_data_template)
     MultiTab *tab = g_new0(MultiTab, 1);
     int pos;
 
+    tab->parent = parent;
     tab->status_stock = NULL;
     tab->widget = multi_tab_filler(parent, tab, user_data_template,
         &tab->user_data, &tab->active_widget, &tab->adjustment);

commit 7e504ab0a6e263bdf6009476dcf5dbdd7c67f199

Fix signal handler cleanup:
Removing signal handler from old window after moving a tab got broken
by 2d9bc1b8174145c1fcae7213c6b22cd77adfd482.

src/multitab.c
@@ -740,7 +740,10 @@ void multi_tab_move_to_new_window(MultiWin *win, MultiTab *tab, int position)
     multi_win_add_tab(win, tab, position, FALSE);
     multi_win_set_geometry_hints_for_tab(win, tab);
     if (multi_tab_to_new_window_handler)
-        multi_tab_to_new_window_handler(win, tab, old_win_destroyed);
+    {
+        multi_tab_to_new_window_handler(win, tab,
+                old_win_destroyed ? NULL : old_win);
+    }
     /* Remove extra references added by multi_tab_remove_from_parent */
     g_object_unref(tab->widget);
 }

src/multitab.h
@@ -75,10 +75,10 @@ typedef void (*MultiTabSelectionHandler) (gpointer user_data, MultiTab * tab);
 
 /* Called when a tab is dragged to a new window; should be used to try to make
  * sure page is same size as other tabs in new window and to update references.
- * old_win_destroyed is TRUE if the tab's previous window was destroyed.
+ * old_win is NULL if the tab's previous window was destroyed.
  */
 typedef void (*MultiTabToNewWindowHandler)(MultiWin *win, MultiTab *tab,
-        gboolean old_win_destroyed);
+        MultiWin *old_win);
 
 /* Called when the zoom factor is changed */
 typedef void (*MultiWinZoomHandler)(gpointer user_data, double zoom_factor,

src/roxterm.c
@@ -970,7 +970,9 @@ static void roxterm_data_delete(ROXTermData *roxterm)
     gwin = roxterm_get_toplevel(roxterm);
     if (gwin && roxterm->win_state_changed_tag)
     {
+        g_debug(">> roxterm_data_delete: win_state_changed");
         g_signal_handler_disconnect(gwin, roxterm->win_state_changed_tag);
+        g_debug("<< roxterm_data_delete: win_state_changed");
     }
     if (roxterm->post_exit_tag)
         g_source_remove(roxterm->post_exit_tag);
@@ -1507,7 +1509,9 @@ static void roxterm_clear_hold_over_uri(ROXTermData *roxterm)
 {
     if (roxterm->hold_over_uri)
     {
+        g_debug(">> roxterm_clear_hold_over_uri");
         g_signal_handler_disconnect(roxterm->widget, roxterm->hold_handler_id);
+        g_debug("<< roxterm_clear_hold_over_uri");
         roxterm->hold_over_uri = FALSE;
     }
 }
@@ -4214,13 +4218,13 @@ void roxterm_launch(const char *display_name, char **env)
 }
 
 static void roxterm_tab_to_new_window(MultiWin *win, MultiTab *tab,
-        gboolean old_win_destroyed)
+        MultiWin *old_win)
 {
     ROXTermData *roxterm = multi_tab_get_user_data(tab);
     ROXTermData *match_roxterm;
     MultiTab *match_tab = multi_win_get_current_tab(win);
-    GtkWindow *old_gwin = old_win_destroyed ? NULL :
-            roxterm_get_toplevel(roxterm);
+    GtkWindow *old_gwin = old_win ?
+            GTK_WINDOW(multi_win_get_widget(old_win)) : NULL;
 
     if (old_gwin)
     {