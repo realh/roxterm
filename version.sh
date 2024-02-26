e7be745caab6055374dde1b0a06802a99dbfeb8b
Tony Houghton <h@realh.co.uk>
2024-02-26 20:21:12 +0000

    Escaping  ~ in version.sh was necessary after all

--- version.sh
+++ version.sh
@@ -3,12 +3,12 @@ cd `dirname "$0"`
 if [ -d ".git" ] ; then
     v=`git describe --match '[0-9]*'`
     v=${v/-/.}
-    v="${v/-/~}"
+    v="${v/-/\~}"
     printf $v | tee version
     if echo $v | grep -qv '~' ; then
         printf $v > release_version
     fi
-elif [ - f version ] ; then
+elif [ -f version ] ; then
     cat version
 else
     cat release_version
