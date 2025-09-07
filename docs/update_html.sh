f7c7445951cedabf7abcd62fca69c2a8fb7b440f
Tony Houghton <h@realh.co.uk>
2025-09-07 16:34:57 +0100

    update_html.sh is no longer needed

--- docs/update_html.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sed -i'' 's/<div id="Middle" class=.clear.>/<div class="Middle clear">/' new_index.html
-sed -i'' 's/<div id="Middle">/<div class="Middle">/' new_index.html
-sed -i'' 's/<div id="Content" class="partialWidth">/<div class="Content partialWidth">/' new_index.html
-sed -i'' 's/<div id="Content">/<div class="Content">/' new_index.html
-sed -i'' 's/href="news.html">/href="#News">/' new_index.html
-sed -i'' 's/href="installation.html">/href="#Installation">/' new_index.html
-sed -i'' 's/href="guide.html">/href="#Guide">/' new_index.html
